c7d6679a937aadc0d0b3ea04ef8f0e3ad6aa24a0
MeganMc123 <55436251+MeganMc123@users.noreply.github.com>
2020-10-06 15:28:52 +0100

    Add files via upload
    WEEK 2

---
+++ Lab/exercise4.js
@@ -0,0 +1,15 @@
+let table1 = "<table>";
+table1 +=	"<tr>";
+table1 +=	"	<th>Company</th>";
+table1 +=	"	<th>Contact</th>";
+table1 +=	"	<th>Country</th>";
+table1 +=	"</tr>";
+			
+table1 +=	"<tr>";
+table1 +=	"	<td>Microsoft</td>";
+table1 +=	"	<td>Bill Gates</td>";
+table1 +=	"	<td>USA</td>";
+table1 +=	"	</tr>";
+table1 +=	"</table>";
+
+document.getElementById("element1").innerHTML = table1;
